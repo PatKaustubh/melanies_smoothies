a2cf4be55d1eda99af811cc5bb08796c88a0c244
Kaustubh Nitin Patil <126760997+PatKaustubh@users.noreply.github.com>
2026-03-20 16:44:45 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 # Import python packages.
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
 
 # Write directly to the app.
 st.title(f"Customise Your Smoothie! :cup_with_straw:")
