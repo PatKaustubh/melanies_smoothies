b18618a63b7a1c496bed712e2d336a1948502854
Kaustubh Nitin Patil <126760997+PatKaustubh@users.noreply.github.com>
2026-03-20 17:49:11 +0530

    Create streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

---
+++ streamlit_app.py
@@ -0,0 +1,84 @@
+# # Import python packages.
+# import streamlit as st
+
+# # Write directly to the app.
+# st.title(f"Customise Your Smoothie! :cup_with_straw:")
+# st.write(
+#   """Choose the fruits you want in your custom smoothie!
+  
+#   """
+# )
+
+
+# option = st.selectbox(
+#     "What is your favorite fruit?",
+#     ("Banana", "Strawberries", "Peaches"),
+# )
+
+# st.write("Your favorite fruit is:", option)
+
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.fruit_options")
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+# Import python packages.
+import streamlit as st
+from snowflake.snowpark.functions import col
+
+# Write directly to the app.
+st.title("Customise Your Smoothie! 🥤")
+st.write(
+    """Choose the fruits you want in your custom smoothie!
+    """
+)
+
+
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be:", name_on_order)
+
+# option = st.selectbox(
+#     "What is your favorite fruit?",
+#     ("Banana", "Strawberries", "Peaches"),
+# )
+
+# st.write("Your favorite fruit is:", option)
+
+
+# Get the active Snowflake session
+cnx=st.connection("snowflake")
+session=cnx.session()
+# session = get_active_session()
+
+# Query the FRUIT_OPTIONS table
+my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
+
+# Display the dataframe in Streamlit
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients ',
+    my_dataframe,
+    max_selections=5
+)
+
+if ingredients_list:
+    # st.write(ingredients_list)
+    # st.text(ingredients_list)
+    ingredients_string = ''
+    for fruit_choosen in ingredients_list:
+        ingredients_string+=fruit_choosen + ' '
+    # st.write(ingredients_string)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                    values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+# st.write(my_insert_stmt)
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
+my_dataframe = session.table("smoothies.public.orders") \
+    .filter(col("ORDER_FILLED") == 0) \
+    .collect()
+
